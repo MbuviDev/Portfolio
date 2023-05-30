3b6a002a6291d936a80eca038e4321bdee9d7711
Meshack <mbuvi.meshack99@gmail.com>
2023-05-30 23:04:59 +0300

    Editing  About file

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function About () {
+    return (
+        <div>
+            <h1>Hey! Am Meshack Mbuvi</h1>
+            <p>Am a fullstack software developer</p>
+
+
+
+        </div>
+    )
+}
+
+export default About;
