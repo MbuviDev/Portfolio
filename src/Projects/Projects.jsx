bcd1b927ea25a8bbf2bed03261a727bac6d8bde1
Meshack <mbuvi.meshack99@gmail.com>
2023-05-30 23:18:40 +0300

    updated Projects

---
+++ src/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Projects () {
+   
+    return (
+        <div>
+            <h1>Projects I have been able to accomplish</h1>
+
+
+        </div>
+    )
+
+}
+
+export default Projects
