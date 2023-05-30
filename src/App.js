1c5f27aa51c47d19bd3bcd9a523a972da77a7865
Meshack <mbuvi.meshack99@gmail.com>
2023-05-30 23:25:21 +0300

    installed react-router

--- src/App.js
+++ src/App.js
@@ -4,54 +4,60 @@ import './App.css';
 function App() {
   return (
    
-    <div className="bg-gray-100 min-h-screen">
-      <header className="bg-white shadow">
-        <div className="container mx-auto px-4">
-          <nav className="flex items-center justify-between flex-wrap py-4">
-            <div className="flex items-center flex-shrink-0 text-gray-900">
-              <span className="font-semibold text-xl">Meshack Mbuvi</span>
-            </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div className="text-sm lg:flex-grow">
-                <a
-                  href="#about"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-600 mr-4"
-                >
-                  About
-                </a>
-                <a
-                  href="#portfolio"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-600 mr-4"
-                > Portfolio
-                </a>
-                <a
-                  href="#contact"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-600"
-                >
-                  Contact
-                </a>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </header>
+    // <div className="bg-gray-100 min-h-screen">
+    //   <header className="bg-white shadow">
+    //     <div className="container mx-auto px-4">
+    //       <nav className="flex items-center justify-between flex-wrap py-4">
+    //         <div className="flex items-center flex-shrink-0 text-gray-900">
+    //           <span className="font-semibold text-xl">Meshack Mbuvi</span>
+    //         </div>
+    //         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    //           <div className="text-sm lg:flex-grow">
+    //             <a
+    //               href="#about"
+    //               className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-600 mr-4"
+    //             >
+    //               About
+    //             </a>
+    //             <a
+    //               href="#portfolio"
+    //               className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-600 mr-4"
+    //             > Portfolio
+    //             </a>
+    //             <a
+    //               href="#contact"
+    //               className="block mt-4 lg:inline-block lg:mt-0 text-gray-900 hover:text-gray-600"
+    //             >
+    //               Contact
+    //             </a>
+    //           </div>
+    //         </div>
+    //       </nav>
+    //     </div>
+    //   </header>
 
-      <section id="hero" className="bg-gray-200 py-20">
-        <div className="container mx-auto px-4">
-          <h1 className="text-4xl font-bold text-center text-gray-900 mb-8">
-            Welcome to My Portfolio!
-          </h1>
-          <p className="text-xl text-center text-gray-700">
-            Am a Software Engineer .
-          </p>
-        </div>
-      </section>
+    //   <section id="hero" className="bg-gray-200 py-20">
+    //     <div className="container mx-auto px-4">
+    //       <h1 className="text-4xl font-bold text-center text-gray-900 mb-8">
+    //         Welcome to My Portfolio!
+    //       </h1>
+    //       <p className="text-xl text-center text-gray-700">
+    //         Am a Software Engineer .
+    //       </p>
+    //     </div>
+    //   </section>
 
-      <section id="about" className="bg-white py-20" />
-        {/* <div className="container mx-auto px-4" > */}
-          <h2 className="text-3xl font-bold">Meshack Mbuvi</h2>
+    //   <section id="about" className="bg-white py-20" />
+    //     {/* <div className="container mx-auto px-4" > */}
+    //       <h2 className="text-3xl font-bold">Meshack Mbuvi</h2>
            
-      </div >  
+    //   </div >  
+
+
+    <div>
+
+      
+    </div>
   );
 }
 
