dcdb91b5e2108da11a3a0b8ffed3644c518acf79
Meshack <mbuvi.meshack99@gmail.com>
2023-05-31 17:10:12 +0300

    edited contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,6 +4,17 @@ function Contact () {
 
     return (
         <div>
+            <h4>Contact me:</h4>
+          <ul>
+            <li>Phone: +2547 68 254 019</li>
+            <li>Email: mbuvi.meshack99@gmail.com</li>
+            <li>Location: Nairobi, Kenya</li>
+          </ul>
+
+          <ul className=''>
+            <li><a href="https://www.linkedin.com/in/mbuvi-meshack-480027228/"><i className='fa fa-linkedin'></i></a></li>
+            <li><a href="https://github.com/MbuviDev"><i className='fa fa-github'></i></a></li>
+          </ul>
 
         </div>
     )
